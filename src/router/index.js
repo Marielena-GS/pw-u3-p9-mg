9061bbbca4045c5bc90ba3c750f114e771e7f9a0
aaaaaa <aaaa@gmail.com>
2026-01-09 19:37:44 -0500

    Taller22

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,34 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import HolaMundo from '@/views/HolaMundo.vue'
+import PokemonView from '@/views/PokemonView.vue'
+import PreguntaView from '@/views/PreguntaView.vue'
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
+
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  }, 
+   {
+    path: '/hola',
+    name: 'hola',
+    component: HolaMundo
+  },
+  {
+    path:'/pokemon',
+    name:'pokemon',
+    component: PokemonView
+  },
+  {
+    path: '/pregunta',
+    name: 'pregunta',
+    component: () => import(/* webpackChunkName: "pregunta" */ '../views/PreguntaView.vue')
   }
 ]
+
+
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
